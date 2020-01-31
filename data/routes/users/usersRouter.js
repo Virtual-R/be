52f4d808373dfad631ceed466c31f00036118149
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-01-30 18:42:06 -0600

    Configured the server. Will be moving all server.use statements to server file to keep the initialization and execution of the server discrete.

--- data/routes/users/usersRouter.js
+++ data/routes/users/usersRouter.js
@@ -3,7 +3,10 @@ const usersModel = require('./usersModel')
 const jwt = require('jsonwebtoken')
 const secrets = require('../../config/secrets')
 const bcrypt = require('bcryptjs')
+const projectsRouter = require('../projects/projectsRouter')
+
 
 const router = express.Router()
+router.use('/:id/projects', projectsRouter)
 
 router.post('/')
