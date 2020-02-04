70a750803400a4dbda519953faec0bcafd87a0c8
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-02-03 19:41:14 -0600

    Added some tests to the projectsRouter.

--- data/routes/projects/projectsRouter.spec.js
+++ data/routes/projects/projectsRouter.spec.js
@@ -15,4 +15,27 @@ describe('projects router tests', () => {
         expect(res.type).toBe('application/json')
     })
 
+    test('get a specific project', async () => {
+        jest.setTimeout(10000)
+        const res = await request(server).get('/api/users/1/projects/1')
+        expect(res.status).toBe(200)
+        expect(res.type).toBe('application/json')
+    })
+
+    test('create a new project', async () => {
+        jest.setTimeout(10000)
+        const payload = {
+            user_id: 1,
+            title: "Dogs, dogs, dogs!",
+            description: "So many dogs! We love dogs. You can pet them.",
+            goal_amount: 10000,
+            amount_received: 1000,
+            funding_completed: false,
+        }
+        const res = await request(server)
+        .post('/api/users/1/projects/')
+        .send(payload)
+        expect(res.status).toBe(201)
+        expect(res.type).toBe('application/json')
+    })
 })
