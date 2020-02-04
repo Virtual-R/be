4e8848b90d395fd162106754d32a146cb74587a3
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-02-03 19:21:07 -0600

    Added routes for the rest of CRUD operations on projectsRouter.

--- data/routes/projects/projectsRouter.js
+++ data/routes/projects/projectsRouter.js
@@ -23,4 +23,44 @@ router.get('/:id', validateProjectId(), async (req, res, next) => {
     }
 })
 
+router.post('/', validateProject(), async (req, res, next) => {
+    try {
+        const project = await projectsModel.add(req.body)
+        res.status(201).json(project)
+    }
+    catch (error) {
+        next(error)
+    }
+})
+
+router.put(':id', validateProject(), authenticate(), validateProjectId(), async (req, res, next) => {
+    const changes = {
+        user_id: req.body.user_id,
+        title: req.body.title,
+        description: req.body.description,
+        goal_amount: req.body.goal_amount,
+        amount_received: req.body.amount_received,
+        funding_completed: req.body.funding_completed,
+    }
+    try {
+        const updates = await projectsModel.update(req.params.id, changes)
+        res.status(200).json(updates)
+    }
+    catch (error) {
+        next(error)
+    }
+})
+
+router.delete('/:id', authenticate(), validateProjectId(), async (req, res, next) => {
+    try {
+        const deletedProject = await projectsModel.remove(req.params.id)
+        if(deletedProject > 0) {
+            res.status(204).json({ message: 'Project was deleted.'})
+        }
+    }
+    catch (error) {
+        next(error)
+    }
+})
+
 module.exports = router;
